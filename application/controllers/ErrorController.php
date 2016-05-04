50268ac11681ee8ffc40be75e3e1ee4db3c2f7d9
Ludan Luan <luan.ludan@hotmail.com>
2016-05-04 22:32:47 +0200

    Fix error controller

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -1,13 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class ErrorController extends CErrorHandler {
+class ErrorController extends LSYii_Controller {
 	
 	public function actionError()
 	{
 	    if($error=Yii::app()->errorHandler->error) {
-	        $this->render('error', $error);
+	        // $this->render('error', $error);
 
 	        Airbrake\Instance::notify($error);
 	    }
+	}
+
+	public function actions() {
+
+		return array(
+			'error' => 'application.controllers.error',
+			);
 	}
 }
